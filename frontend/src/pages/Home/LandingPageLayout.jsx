3cad0f3405cb02feb2a1505e750b555cf070ead5
Webstudio123 <backendAbhishek@gmail.com>
2024-11-29 12:29:17 +0545

    change

---
+++ frontend/src/pages/Home/LandingPageLayout.jsx
@@ -0,0 +1,29 @@
+import Banner from "../Home/Banner";
+import Section01 from "./Section01";
+import Section02 from "./Section02";
+import Section03 from "./section03";
+import FAQSection from "./Faq";
+import TrekOfTheYear from "./TrekOftheYear";
+import ReviewSection from "./Review";
+import VideoBlog from "./VideoBlog";
+
+const LandingPageLayout = ()=>{
+    return(
+        <>
+           <>
+            <Banner />
+              <Section01 />
+              <Section02 />
+              <Section03 />
+              <FAQSection />
+              <TrekOfTheYear />
+              <ReviewSection />
+              <VideoBlog />
+      
+          
+            </>
+        </>
+    )
+}
+
+export default LandingPageLayout
